c30eb8ae1ade3aa9096fa3a5227e03ca3cf9aaf3
Anurag-3112 <ak7546824@gmail.com>
2026-05-06 16:44:09 +0530

    project

---
+++ script.js
@@ -0,0 +1,16 @@
+function formatJSON() {
+const input = document.getElementById("input").value;
+const output = document.getElementById("output");
+const error = document.getElementById("error");
+
+try {
+const parsed = JSON.parse(input);
+output.value = JSON.stringify(parsed, null, 2);
+error.textContent = "Valid JSON";
+error.style.color = "#00744f";
+} catch (e) {
+error.textContent = "Invalid JSON";
+error.style.color = "#d40000";
+output.value = "";
+}
+}
